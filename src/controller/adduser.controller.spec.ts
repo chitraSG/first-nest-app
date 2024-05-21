4e90146d597f60c914e3a12ce10a761dcf2ba68c
ChitraSingh <chitra.singh@studiographene.com>
2024-05-21 17:50:54 +0530

    feat:swagger implementation

--- src/controller/adduser.controller.spec.ts
+++ src/controller/adduser.controller.spec.ts
@@ -1,8 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from './entity/user.entity';
-import { UserService } from './user.service';
+import { User } from '../entity/user.entity';
+import { UserService } from '../service/user.service';
 
 describe('UserService (integration)', () => {
   let userService: UserService;
--- src/module/user.module.ts
+++ src/module/user.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entity/user.entity';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
+import { User } from '../entity/user.entity';
+import { UserService } from '../service/user.service';
+import { UserController } from '../controller/user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
